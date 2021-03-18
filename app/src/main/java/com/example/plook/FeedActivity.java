25c24e8af00721ce68ecdb2507a4f09fc735ceff
Juho Laukka <t9laju04@students.oamk.fi>
2021-03-18 12:47:08 +0200

    Feed activity ja sen layout.xml lisätty.

---
+++ app/src/main/java/com/example/plook/FeedActivity.java
@@ -0,0 +1,14 @@
+package com.example.plook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
