8097f65b70834db3c3275e53d68635f9391d9246
Iikka <iikka.kuha@gmail.com>
2021-03-19 15:30:45 +0200

    Changed the project structure com -> co removed example folders

--- app/src/main/java/co/plook/ImageUploadActivity.java
+++ app/src/main/java/co/plook/ImageUploadActivity.java
@@ -1,15 +1,16 @@
-package com.example.plook;
+package co.plook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Welcome extends AppCompatActivity
+public class ImageUploadActivity extends AppCompatActivity
 {
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_image_upload);
     }
 }
