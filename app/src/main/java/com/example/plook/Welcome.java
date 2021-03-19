2243ef3860d5c4bcf734bc42ce9dc3c58b85d22b
n9peje00 <70717377+n9peje00@users.noreply.github.com>
2021-03-19 14:57:13 +0200

    Aloitusnäyttö aluilleen ja väliaikanen logo.

--- app/src/main/java/com/example/plook/Welcome.java
+++ app/src/main/java/com/example/plook/Welcome.java
@@ -9,7 +9,6 @@ public class Welcome extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
