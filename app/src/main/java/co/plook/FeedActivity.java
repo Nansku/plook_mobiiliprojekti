b66ef0ecbf90ebd730036dfbefc37050f0ecfc22
Juho Laukka <t9laju04@students.oamk.fi>
2021-03-22 15:31:53 +0200

    Connection to Firestore works now!

--- app/src/main/java/co/plook/FeedActivity.java
+++ app/src/main/java/co/plook/FeedActivity.java
@@ -1,16 +1,24 @@
 package co.plook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity
 {
-    int postAmount = 5;
+    ViewGroup content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,12 +26,37 @@ public class FeedActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        ViewGroup content = findViewById(R.id.feed_content);
+        content = findViewById(R.id.feed_content);
 
-        for(int i = 0; i < postAmount; i++)
+        // Access Cloud firebase.
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>()
         {
-            View child = getLayoutInflater().inflate(R.layout.layout_feed_post, content, false);
-            content.addView(child);
-        }
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task)
+            {
+                if(task.isSuccessful())
+                {
+                    for(QueryDocumentSnapshot document : task.getResult())
+                    {
+                        addPost(document.getData());
+                    }
+                }
+                else
+                    System.out.println(task.getException());
+            }
+        });
+    }
+
+    private void addPost(Map data)
+    {
+        View child = getLayoutInflater().inflate(R.layout.layout_feed_post, content, false);
+        content.addView(child);
+
+        TextView textView_caption = child.findViewById(R.id.post_caption);
+        TextView textView_description = child.findViewById(R.id.post_description);
+
+        //textView_caption.setText(data.get());
     }
 }
