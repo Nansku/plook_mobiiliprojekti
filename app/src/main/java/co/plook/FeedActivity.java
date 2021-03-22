81575403d08afdd484ff30f2ad7c1e1eb556e913
Juho Laukka <t9laju04@students.oamk.fi>
2021-03-22 17:06:37 +0200

    Loading images from the database works now!

--- app/src/main/java/co/plook/FeedActivity.java
+++ app/src/main/java/co/plook/FeedActivity.java
@@ -3,22 +3,34 @@ package co.plook;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity
 {
-    ViewGroup content;
+    private ViewGroup content;
+
+    private FirebaseFirestore db;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,8 +40,10 @@ public class FeedActivity extends AppCompatActivity
 
         content = findViewById(R.id.feed_content);
 
-        // Access Cloud firebase.
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Access Firebase.
+        db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
 
         db.collection("posts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>()
         {
@@ -56,11 +70,26 @@ public class FeedActivity extends AppCompatActivity
 
         TextView textView_caption = child.findViewById(R.id.post_caption);
         TextView textView_description = child.findViewById(R.id.post_description);
+        ImageView imageView_image = child.findViewById(R.id.image);
 
         String caption = data.get("caption").toString();
         String description = data.get("description").toString();
+        String imageUrl = data.get("url").toString();
 
         textView_caption.setText(caption);
         textView_description.setText(description);
+
+        StorageReference gsReference = storage.getReferenceFromUrl(imageUrl);
+
+        gsReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
+        {
+            @Override
+            public void onSuccess(Uri uri)
+            {
+                String imageUri = uri.toString();
+
+                Glide.with(getApplicationContext()).load(imageUri).into(imageView_image);
+            }
+        });
     }
 }
