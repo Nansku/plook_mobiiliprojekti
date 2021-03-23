0da38cc33f1c532901d914040d6bda80496cd3b6
Kristian Ängeslevä <kristian.angesleva@gmail.com>
2021-03-23 13:58:36 +0200

    Upload aluillaan

--- app/src/main/java/co/plook/ImageUploadActivity.java
+++ app/src/main/java/co/plook/ImageUploadActivity.java
@@ -1,16 +1,62 @@
 package co.plook;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ImageUploadActivity extends AppCompatActivity
 {
+
+    private ImageView profilePic;
+    public Uri imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_upload);
+        profilePic = findViewById(R.id.profilePic);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
+
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!= null && data.getData()!= null){
+            imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+
     }
 }
