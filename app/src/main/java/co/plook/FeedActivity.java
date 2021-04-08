1b94cc26fd3624a2e10810a09c21fe26387b68c7
Iikka <iikka.kuha@gmail.com>
2021-04-08 17:42:06 +0300

    (TEST) FeedActivity now uses the drawer layout

--- app/src/main/java/co/plook/FeedActivity.java
+++ app/src/main/java/co/plook/FeedActivity.java
@@ -41,7 +41,7 @@ public class FeedActivity extends ParentActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        setContentView(R.layout.drawer);
 
         context = getApplicationContext();
 
