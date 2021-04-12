a920d583c3a1911197292743ff3ad74a9c87aede
Iikka <iikka.kuha@gmail.com>
2021-04-12 19:37:30 +0300

    changed postActivity to inherit drawer

--- app/src/main/java/co/plook/PostActivity.java
+++ app/src/main/java/co/plook/PostActivity.java
@@ -43,7 +43,7 @@ public class PostActivity extends ParentActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post);
+        getLayoutInflater().inflate(R.layout.activity_post, contentGroup);
 
         context = getApplicationContext();
 
